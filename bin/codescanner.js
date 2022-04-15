354ae52eb2ee2fa4a76099cc0cf4c064498efffc
Green_Lab <greencoder001@outlook.com>
2022-04-16 00:31:33 +0200

    Added scanmy command

--- bin/codescanner.js
+++ bin/codescanner.js
@@ -96,6 +96,37 @@ function scandDir (regex) {
     })
 }
 
+function scandDirMy (regex) {
+    const files = readdirSyncRecursive(process.cwd())
+    console.log('------------------------------------------------------------------------------------------------------------------------')
+    files.forEach(file => {
+        // Don't scan ignored files
+        if (ignoreFileEndings.some(ending => file.endsWith(ending))) {
+            return
+        }
+
+        // Don't scan node_modules
+        if (file.includes('node_modules')) {
+            return
+        }
+
+        try {
+            const filecontent = fs.readFileSync(file, 'utf8').toString('utf8')
+            const _matches = [...filecontent.matchAll(regex)]
+
+            for (const match of _matches) {
+                // Get line number
+                const line = filecontent.substring(0, match.index).split('\n').length
+
+                console.log(`${_sessionname}, I found a match in ${file} at line ${line}!`)
+                console.log('------------------------------------------------------------------------------------------------------------------------')
+            }
+        } catch (e) {
+            console.log(`${_sessionname}, we have a problem! Seems like ${file} is too large to scan!`)
+        }
+    })
+}
+
 const rl = require('readline-sync')
 let cmd = ''
 while (cmd !== 'exit') {
@@ -111,6 +142,7 @@ while (cmd !== 'exit') {
         console.log('  exit - exit the program')
         console.log('  scan <RegEx> - scan the current directory for code that matches the RegEx')
         console.log('  iscan <RegEx> - scan the current directory for code that matches the RegEx without case sensitivity')
+        console.log('  scanmy <RegEx> - scan the current directory for code that matches the RegEx, but ignore everything which includes node_modules')
         console.log()
         console.log('Example:')
         console.log('  scan (.*?); // scan for any code that ends with a semicolon')
@@ -127,6 +159,12 @@ while (cmd !== 'exit') {
         const argsf = args.join(' ')
         const regex = new RegExp(argsf, 'gi')
         scandDir(regex)
+    } else if (cmd.startsWith('scanmy')) {
+        const args = cmd.split(' ')
+        args.shift()
+        const argsf = args.join(' ')
+        const regex = new RegExp(argsf, 'g')
+        scandDirMy(regex)
     } else {
         console.log('Unknown command: ' + cmd)
         console.log('Type "help" for a list of available commands.')
